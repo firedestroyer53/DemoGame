d9da0e39a0bbeb42179d588b2f66ec7e3665e612
firedestroyer53 <firedestroyer53@gmail.com>
2023-02-18 13:42:50 -0500

    Add project files.

---
+++ environmental/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace environmental
+{
+    class Program
+    {
+        public class Character
+        {
+            public int Strength { get; set; }
+            public int Intelligence { get; set; }
+            public int Agility { get; set; }
+            public int Reputation { get; set; }
+            public string Name { get; set; }
+            public Character(int strength, int agility, int intelligence, string name, int reputation)
+            {
+                Strength = strength;
+                Agility = agility;
+                Intelligence = intelligence;
+                Name = name;
+                Reputation = reputation;
+            }
+
+        }
+        public class NPC
+        {
+            public int LikabilityScore { get; set; }
+            public string[] Dialogue { get; set; }
+            public int Reputation { get; set; }
+            public bool IsAllyable { get; set; }
+            public string Name { get; set; }
+
+            public NPC(int likabilityScore, string[] dialogue, string name, int reputation, bool isAllyable)
+            {
+                LikabilityScore = likabilityScore;
+                Dialogue = dialogue;
+                Reputation = reputation;
+                Name = name;
+                IsAllyable = isAllyable;
+            }
+        }
+
+        public class Enemy
+        {
+            public int HP { get; set; }
+            public int Attack { get; set; }
+            public string Name { get; set; }
+
+            public Enemy(int hp, int attack, string name)
+            {
+                HP = hp;
+                Attack = attack;
+                Name = name;
+            }
+        }
+
+        static Character CreateCharacter()
+        {
+            const int NUM_INPUTS = 3;
+
+            // Input loop
+            Character player = null;
+            while (player == null)
+            {
+                try
+                {
+                    // Read input and parse to integers
+                    int[] inputs = Console.ReadLine().Split(',')
+                        .Select(int.Parse).ToArray();
+
+                    // Check number of inputs
+                    if (inputs.Length != NUM_INPUTS)
+                    {
+                        Console.WriteLine($"Invalid number of inputs. Please enter {NUM_INPUTS} inputs.");
+                        continue;
+                    }
+
+                    // Check skill point total
+                    int totalSkillPoints = inputs.Sum();
+                    if (totalSkillPoints != 20)
+                    {
+                        Console.WriteLine("Invalid skill point total. Please assign exactly 20 skill points.");
+                        continue;
+                    }
+
+                    // Create player character
+                    Console.WriteLine("What is your name?");
+                    player = new Character(inputs[0], inputs[1], inputs[2], Console.ReadLine(), 50);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input format. Please enter integers separated by commas.");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Input value is too large. Please enter a smaller value.");
+                }
+            }
+
+            return player;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to [INSERT GAME NAME HERE].");
+            Console.WriteLine("Please input your character's stats (strength, agility, intelligence) in the format of an array (e.g. 1,2,3).");
+            Console.WriteLine("You need to assign 20 skill points.");
+            
+            float LevelScale = 1.0f;
+
+            Character player = CreateCharacter();
+
+            Console.WriteLine($"{player.Name} has a strength stat of {player.Strength}, an agility stat of {player.Agility}, and an intelligence stat of {player.Intelligence}.");
+
+
+        }
+    }
+}
---
+++ environmental/Robot.cs
@@ -0,0 +1,127 @@
+﻿namespace environmental;
+
+public class Robot
+{
+    public static void Robot1()
+    {
+        const int GridHeight = 30;
+        const int GridWidth = 30;
+
+        char robotFacing = 'n';
+        int robotRow = 0;
+        int robotColumn = 0;
+        int prevRow = -1;
+        int prevColumn = -1;
+
+        int[,] grid = new int[GridHeight, GridWidth];
+
+        Console.WriteLine("Give commands to the robot. 'F' for forward, 'L' for turn left, and 'R' for turn right. It should be laid out like this: FLLFFRF. Keep in mind that the robot starts in the top left corner of the grid.");
+        string commands = Console.ReadLine();
+
+        foreach (char c in commands)
+        {
+            switch (c)
+            {
+                case 'F':
+                    Move(robotFacing);
+                    break;
+                case 'L':
+                    switch (robotFacing)
+                    {
+                        case 'n':
+                            robotFacing = 'w';
+                            break;
+                        case 's':
+                            robotFacing = 'e';
+                            break;
+                        case 'e':
+                            robotFacing = 'n';
+                            break;
+                        case 'w':
+                            robotFacing = 's';
+                            break;
+                    }
+                    break;
+                case 'R':
+                    switch (robotFacing)
+                    {
+                        case 'n':
+                            robotFacing = 'e';
+                            break;
+                        case 's':
+                            robotFacing = 'w';
+                            break;
+                        case 'e':
+                            robotFacing = 's';
+                            break;
+                        case 'w':
+                            robotFacing = 'n';
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine($"Invalid command: {c}");
+                    break;
+            }
+
+        }
+
+        Console.WriteLine($"The robot ended up in {robotRow}, {robotColumn}");
+
+        void Move(char direction)
+        {
+            int newRow = robotRow;
+            int newColumn = robotColumn;
+
+            switch (direction)
+            {
+                case 'n':
+                    newRow++;
+                    break;
+                case 's':
+                    newRow--;
+                    break;
+                case 'e':
+                    newColumn++;
+                    break;
+                case 'w':
+                    newColumn--;
+                    break;
+            }
+
+            if (newRow < 0 || newRow >= GridHeight || newColumn < 0 || newColumn >= GridWidth)
+            {
+                // invalid move
+                return;
+            }
+
+            grid[robotRow, robotColumn] = 0;
+            grid[newRow, newColumn] = 1;
+            robotRow = newRow;
+            robotColumn = newColumn;
+
+            for (int i = 0; i < GridHeight; i++)
+            {
+                for (int j = 0; j < GridWidth; j++)
+                {
+                    if (i == robotRow && j == robotColumn)
+                    {
+                        Console.Write('R');
+                    }
+                    else if (grid[i, j] == 1)
+                    {
+                        Console.Write('X');
+                    }
+                    else
+                    {
+                        Console.Write('-');
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+    }
+}
